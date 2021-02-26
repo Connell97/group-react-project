6c528fd5d9d3578543b4b8f0489ba84f07079e52
Matt <m.connell97@hotmail.co.uk>
2021-02-26 16:18:28 +0000

    Added CSS for Tweet

--- src/components/mainFeed/profileBar/ProfileBar.js
+++ src/components/mainFeed/profileBar/ProfileBar.js
@@ -3,10 +3,14 @@ import './ProfileBar.css';
 const ProfileBar = (props) => {
     return (
         <div className = "profile-bar">
-            <img src = {props.profileInfo.profilePic} alt = "Profile pic"/>
-            <p className = "profile-name">{props.profileInfo.profileName}</p>
-            <p className = "handle">{props.profileInfo.handle}</p>
-            <p className = "timestamp">{props.profileInfo.timestamp}</p>
+            <div className = "profile-pic">
+                <img src = {props.profileInfo.profilePic} alt = "Profile pic"/>
+            </div>
+            <div className = "profile-info">
+                <p className = "profile-name">{props.profileInfo.profileName}</p>
+                <p className = "handle">{props.profileInfo.handle}</p>
+                <p className = "timestamp">{props.profileInfo.timestamp}</p>
+            </div>
         </div>
     )
 }
